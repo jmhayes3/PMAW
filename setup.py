cee117da9bcb9b4f22b8be9a568c7402f0a7a5a8
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2022-01-17 04:38:26 -0600

    add project files

---
+++ setup.py
@@ -0,0 +1,34 @@
+import re
+from codecs import open
+from os import path
+
+from setuptools import find_packages, setup
+
+PACKAGE = "pmaw"
+with open(path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8") as fp:
+    README = fp.read()
+with open(path.join(path.abspath(path.dirname(__file__)), PACKAGE, "const.py"), encoding="utf-8") as fp:
+    VERSION = re.search('__version__ = "([^"]+)"', fp.read()).group(1)
+
+setup(
+    name=PACKAGE,
+    author="jmhayes3",
+    author_email="22490346+jmhayes3@users.noreply.github.com",
+    python_requires="~=3.7",
+    classifiers=[
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    description="Python Messari API Wrapper",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    license="MIT License",
+    version=VERSION,
+)
