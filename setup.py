ccaf8bcb854b31a2f2a0cc9062b1c9e87a596e98
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2022-09-22 02:35:36 -0500

    update project files

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import re
+import pkg_resources
+
 
 from codecs import open
 from os import path
@@ -18,16 +20,9 @@ setup(
     name=PACKAGE,
     author="jmhayes3",
     author_email="22490346+jmhayes3@users.noreply.github.com",
-    python_requires="~=3.7",
     classifiers=[
         "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
     ],
     description="Python Messari API Wrapper",
     long_description=README,
@@ -35,4 +30,5 @@ setup(
     packages=find_packages(exclude=["tests", "tests.*"]),
     license="MIT License",
     version=VERSION,
+    extras_require={"dev": ["pytest"]},
 )
